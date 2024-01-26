9fe9db7603a32f0fc34bb51f5b71aaae4ab9a98b
Arunendra Singh <singharunendra978@gmail.com>
2024-01-26 16:54:10 +0530

    Addeds size and color prop to make size and color dynamially.

--- src/RatingStar.js
+++ src/RatingStar.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-function RatingStar({ stars = 5 }) {
+function RatingStar({ stars = 5, size=50, color="yellow", setStarRating }) {
   // Set status for rating
   const [rating, setRating] = useState(0);
   const [tempRting, setTempRating] = useState(null);
@@ -10,8 +10,10 @@ function RatingStar({ stars = 5 }) {
     console.log("Rating is: ", rating);
     if (rating === Number(value)) {
       setRating(rating - 1);
+      setStarRating(rating - 1)
     } else {
       setRating(value);
+      setStarRating(value)
     }
   };
 
@@ -28,6 +30,8 @@ function RatingStar({ stars = 5 }) {
             selected={index > rating - 1 ? false : true}
             tempSeleted={index + 1 > tempRting ? false : true}
             onHover={handleHoverEffect}
+            size={size}
+            color={color}
           />
         ))}
       </div>
@@ -36,13 +40,13 @@ function RatingStar({ stars = 5 }) {
   );
 }
 
-function Star({ ratingValue, handleClick, onHover, selected, tempSeleted }) {
+function Star({ ratingValue, handleClick, onHover, selected, tempSeleted, size,color }) {
   return (
     <span
       role="button"
       style={{
-        height: "40px",
-        width: "40px",
+        height: `${size}px`,
+        width: `${size}px`,
         display: "block",
         cursor: "pointer",
       }}
@@ -54,8 +58,8 @@ function Star({ ratingValue, handleClick, onHover, selected, tempSeleted }) {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
-          fill="#000"
-          stroke="#000"
+          fill={`${color}`}
+          stroke={`${color}`}
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
         </svg>
@@ -64,7 +68,7 @@ function Star({ ratingValue, handleClick, onHover, selected, tempSeleted }) {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke="#000"
+          stroke={`${color}`}
         >
           <path
             strokeLinecap="round"
